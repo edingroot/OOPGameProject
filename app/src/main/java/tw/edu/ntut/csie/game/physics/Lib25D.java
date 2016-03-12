3873363f04424048e27f4dc4c5cf60556a44465d
David Lin <voyage01090@gmail.com>
2016-03-12 16:11:27 +0800

    explaination of the function

--- app/src/main/java/tw/edu/ntut/csie/game/physics/Lib25D.java
+++ app/src/main/java/tw/edu/ntut/csie/game/physics/Lib25D.java
@@ -4,13 +4,13 @@ public class Lib25D {
 
     /**
      * Calculate horizontal displacement adjustment of 2.5D
+     *
      * @param E distance of eye to the nearest object(dObject = 0)
      * @param D distance of the object
      * @param M horizontal movement in 3D
      * @return the new deltaX
      */
     public static double horizontalMoveAdj(double E, double D, double M) {
-
         double result;
 
         result = M - (M * D) / (E+D);
@@ -18,8 +18,15 @@ public class Lib25D {
         return result;
     }
 
+    /**
+     * Calculate vertical displacement adjustment of 2.5D
+     *
+     * @param E distance of eye to the nearest object(dObject = 0)
+     * @param Eh distance of eye to ground
+     * @param F vertical movement in 3D
+     * @return
+     */
     public static double verticalMoveAdj(double E, double Eh, double F) {
-
         double result;
 
         double distance = E + F;
@@ -32,8 +39,15 @@ public class Lib25D {
         return result;
     }
 
+    /**
+     * Calculate height adjustment of 2.5D
+     *
+     * @param E distance of eye to the nearest object(dObject = 0)
+     * @param H height of the object
+     * @param F vertical movement in 3D
+     * @return
+     */
     public static double heightAdj(double E, double H, double F) {
-
         double result;
 
         result = E * H / (E + F);
