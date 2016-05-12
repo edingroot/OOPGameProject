7c70882f26e14ec5c1a63cbf36bbc97575f880d6
張霽 <edingroot@gmail.com>
2016-05-13 00:00:43 +0800

    Styling StateReady.

--- app/src/main/java/tw/edu/ntut/csie/game/state/StateReady.java
+++ app/src/main/java/tw/edu/ntut/csie/game/state/StateReady.java
@@ -11,18 +11,9 @@ import tw.edu.ntut.csie.game.extend.ButtonEventHandler;
 
 public class StateReady extends AbstractGameState {
 
-    private MovingBitmap _helpInfo;
-    private MovingBitmap _aboutInfo;
     private MovingBitmap _background;
-
-    private BitmapButton _exitButton;
-    private BitmapButton _helpButton;
-    private BitmapButton _menuButton;
-    private BitmapButton _aboutButton;
     private BitmapButton _startButton;
-
-    private boolean _showHelp;
-    private boolean _showAbout;
+    private BitmapButton _exitButton;
 
     public StateReady(GameEngine engine) {
         super(engine);
@@ -30,80 +21,13 @@ public class StateReady extends AbstractGameState {
 
     @Override
     public void initialize(Map<String, Object> data) {
-        addGameObject(_helpInfo = new MovingBitmap(R.drawable.help_info));
-        addGameObject(_background = new MovingBitmap(R.drawable.state_ready));
-        addGameObject(_aboutInfo = new MovingBitmap(R.drawable.about_info));
+        addGameObject(_background = new MovingBitmap(R.drawable.state_ready_bg));
         initializeStartButton();
         initializeExitButton();
-        initializeMenuButton();
-        initializeHelpButton();
-        initializeAboutButton();
-        setVisibility(false, false);
     }
 
-    /**
-     * ��l�ơyAbout�z�����s�C
-     */
-    // �}�o²��
-    private void initializeAboutButton() {
-        addGameObject(_aboutButton = new BitmapButton(R.drawable.about, R.drawable.about_pressed, 465, 270));
-        _aboutButton.addButtonEventHandler(new ButtonEventHandler() {
-            @Override
-            public void perform(BitmapButton button) {
-                setVisibility(false, true);
-            }
-        });
-        addPointerEventHandler(_aboutButton);
-    }
-
-    /**
-     * ��l�ơyHelp�z�����s�C
-     */
-    // �C������
-    private void initializeHelpButton() {
-        addGameObject(_helpButton = new BitmapButton(R.drawable.help, R.drawable.help_pressed, 465, 220));
-        _helpButton.addButtonEventHandler(new ButtonEventHandler() {
-            @Override
-            public void perform(BitmapButton button) {
-                setVisibility(true, false);
-            }
-        });
-        addPointerEventHandler(_helpButton);
-    }
-
-    /**
-     * ��l�ơyMenu�z�����s�C
-     */
-    private void initializeMenuButton() {
-        addGameObject(_menuButton = new BitmapButton(R.drawable.menu, R.drawable.menu_pressed, 465, 320));
-        _menuButton.addButtonEventHandler(new ButtonEventHandler() {
-            @Override
-            public void perform(BitmapButton button) {
-                setVisibility(false, false);
-            }
-        });
-        addPointerEventHandler(_menuButton);
-    }
-
-    /**
-     * ��l�ơyExit�z�����s�C
-     */
-    private void initializeExitButton() {
-        addGameObject(_exitButton = new BitmapButton(R.drawable.exit, R.drawable.exit_pressed, 465, 320));
-        _exitButton.addButtonEventHandler(new ButtonEventHandler() {
-            @Override
-            public void perform(BitmapButton button) {
-                _engine.exit();
-            }
-        });
-        addPointerEventHandler(_exitButton);
-    }
-
-    /**
-     * ��l�ơyStart�z�����s�C
-     */
     private void initializeStartButton() {
-        addGameObject(_startButton = new BitmapButton(R.drawable.start, R.drawable.start_pressed, 465, 170));
+        addGameObject(_startButton = new BitmapButton(R.drawable.btn_start, R.drawable.btn_start_pressed, 370, 108));
         _startButton.addButtonEventHandler(new ButtonEventHandler() {
             @Override
             public void perform(BitmapButton button) {
@@ -113,33 +37,23 @@ public class StateReady extends AbstractGameState {
         addPointerEventHandler(_startButton);
     }
 
+    private void initializeExitButton() {
+        addGameObject(_exitButton = new BitmapButton(R.drawable.btn_exit, R.drawable.btn_exit_pressed, 388, 243));
+        _exitButton.addButtonEventHandler(new ButtonEventHandler() {
+            @Override
+            public void perform(BitmapButton button) {
+                _engine.exit();
+            }
+        });
+        addPointerEventHandler(_exitButton);
+    }
+
     @Override
     public void pause() {
     }
 
     @Override
     public void resume() {
-    }
-
-    /**
-     * �]�w�e���W���ǹϤ�����ܡA���ǹϤ������áC
-     *
-     * @param showHelp  ���Help�e��
-     * @param showAbout ���About�e��
-     */
-    private void setVisibility(boolean showHelp, boolean showAbout) {
-        _showHelp = showHelp;
-        _showAbout = showAbout;
-        boolean showMenu = !_showAbout && !_showHelp;
-        _helpInfo.setVisible(_showHelp);
-        _aboutInfo.setVisible(_showAbout);
-        _background.setVisible(showMenu);
-
-        _exitButton.setVisible(showMenu);
-        _helpButton.setVisible(showMenu);
-        _aboutButton.setVisible(showMenu);
-        _startButton.setVisible(showMenu);
-        _menuButton.setVisible(!showMenu);
     }
 }
 
